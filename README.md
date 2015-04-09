86b2b06e5f7ac0921c1e26349115e3b67fe063ed
Ian Adam Naval <ianonavy@gmail.com>
2015-04-09 04:00:28 -0400

    Add basic hacky version that just works

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 passweb
 =======
 
-Simple Web interface for `pass`.
+Simple Web interface for `pass`. Designed to run on a single computer, and
+assumes that gpg-agent has currently cached your PGP passphrase. If you need
+remote access to your password and your PGP key is locked, you'll have to find
+some way to SSH into your computer and unlock it with the command line (via
+a device you own like a smartphone! Don't enter your PGP passphrase on someone
+else's computer!) No, I will not implement some hacky thing that lets you enter
+your PGP passphrase on some public computer. You should never do that.
 
+## Usage (after cloning)
+
+Generate a private key and self-signed cert:
+
+	cd passweb
+    openssl genrsa -out passweb.key 4096
+    openssl req -new -x509 -key passweb.key -out passweb.cert -days 1095
+
+Create a secret key for sessions:
+
+	cd passweb
+    pwgen 128 1 > secret.txt
+
+Store the password for passweb in pass (duh)
+
+    pass insert passweb
+	(enter password here)
+
+## Issues
+
+Doesn't work with passwords stored in folders. You'll have to manually
---
+++ passweb/server.py
@@ -0,0 +1,99 @@
+# Tread with caution
+# This code was writen in 15 minutes
+
+from flask import Flask, request, url_for, render_template, redirect
+from flask.ext.login import login_required, LoginManager, login_user, \
+    logout_user
+import subprocess
+
+app = Flask(__name__)
+with open('secret.txt') as secret:
+    app.config['SECRET_KEY'] = secret.read()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+class User(object):
+    """Dummy user class for flask-login"""
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return 'admin'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User()
+
+
+def get_pass(name):
+    ret = subprocess.check_output(
+        "pass {} | head -n1".format(name), shell=True)
+    return ret[:-1].decode('utf-8')  # strip newline
+
+
+@app.route('/password/')
+@login_required
+def password():
+    name = request.args.get('name', 'ls')
+    return subprocess.check_output(
+        "pass {}".format(name),
+        shell=True).decode('utf-8').replace('\n', '<br />')
+
+
+@app.route('/')
+@login_required
+def index():
+    names = subprocess.check_output("pass ls", shell=True).decode('utf-8').split('\n')
+
+    # hack to parse out of 'pass ls' into URLs
+    prefix = []  # directory stack
+    for i, name in enumerate(names):
+        if '[01;34m' in name:  # directory detected by the color escapes... ugh
+            # strip color sequences and ESC character
+            name = name.replace('[01;34m', '').replace('[00m', '').replace('\x1b', '')
+            names[i] = name  # store in original output
+            _, name = name.rsplit("── ")  # parse out the name of the directory
+            prefix.append(name.strip())  # add to list
+        else:
+            if "── " in name:
+                _, part_to_urlify = name.split("── ")  # parse out name
+                # build the full name with all the directories
+                get_param = "/".join(prefix) + "/" + part_to_urlify
+                # get the URL for displaying the password
+                url = url_for('password', name=get_param)
+                # hot swap it in with an anchor tag... so clean!
+                names[i] = name.replace(
+                    part_to_urlify,
+                    "<a href='{}'>{}</a>".format(url, part_to_urlify))
+            if "└" in name and prefix:  # last thing in directory
+                prefix.pop()  # go up one level
+    return render_template('index.html', names=names)
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        if request.form['password'] == get_pass('passweb'):
+            login_user(User())
+        return redirect(request.args.get("next") or url_for("index"))
+    return render_template("login.html")
+
+
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=False, ssl_context=('passweb.crt', 'passweb.key'))
